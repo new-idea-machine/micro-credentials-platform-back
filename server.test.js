727db4e67c2d066c2e172bd98effc7db29884fc3
Kevin Woodman <kwoodman1970@yahoo.ca>
2024-01-26 21:13:42 -0700

    Changed order of tests in server.test.js; began documenting server.test.js

--- server.test.js
+++ server.test.js
@@ -1,4 +1,17 @@
 /*
+List of Tests
+=============
+
+User POST Tests
+---------------
+
+1. Register a new learner user with a bad e-mail address.
+2. Register a new learner user with a bad password.
+3. Register a new learner user.
+4. Re-register the same learner user.
+*/
+
+/*
 When jest supports importing modules, the following code fragment can be
 used:
 
@@ -43,36 +56,18 @@ async function sendRequest(data, response, result) {
   return;
 }
 
-/*****************************************************************************/
-
-test("Register New Learner User", async function() {
-  let response = null;
-  let result = null;
-
-  sendRequest(learnerUserData, response, result);
-
-  expect(response?.ok).toBe(true);
-  expect(["string", "number"].indexOf(typeof result?.userUID)).toBeGreaterThanOrEqual(0);
-  expect(typeof result?.msg).toBe("undefined");
-});
-
-/*****************************************************************************/
-
-test("Re-Register New Learner User", async function() {
-  let response = null;
-  let result = null;
-
-  sendRequest(learnerUserData, response, result);
-
-  expect(response?.ok).toBe(false);
-  expect(typeof result?.msg).toBe("string");
-  expect(typeof result?.userUID).toBe("undefined");
-});
-
-/*****************************************************************************/
+// ============================================================================
+// USER POST TESTS
+// ============================================================================
 
 test("Register New Learner User (Bad E-mail Address)", async function() {
   const badData = {...learnerUserData};
+
+  /*
+  TEST 1:  Register a new learner user with a bad e-mail address.
+
+  EXPECTED RESULT:  Fail (status 406).
+  */
 
   badData.userInfo.email = "Bad e-mail address";
 
@@ -82,6 +77,7 @@ test("Register New Learner User (Bad E-mail Address)", async function() {
   sendRequest(badData, response, result);
 
   expect(response?.ok).toBe(false);
+  expect(response?.status).toBe(406);
   expect(typeof result?.msg).toBe("string");
   expect(typeof result?.userUID).toBe("undefined");
 });
@@ -91,6 +87,12 @@ test("Register New Learner User (Bad E-mail Address)", async function() {
 test("Register New Learner User (Bad Password)", async function() {
   const badData = {...learnerUserData};
 
+  /*
+  TEST 2:  Register a new learner user with a bad password.
+
+  EXPECTED RESULT:  Fail (status 406).
+  */
+
   badData.password = null;
 
   let response = null;
@@ -99,6 +101,47 @@ test("Register New Learner User (Bad Password)", async function() {
   sendRequest(badData, response, result);
 
   expect(response?.ok).toBe(false);
+  expect(response?.status).toBe(406);
+  expect(typeof result?.msg).toBe("string");
+  expect(typeof result?.userUID).toBe("undefined");
+});
+
+/*****************************************************************************/
+
+test("Register New Learner User", async function() {
+  /*
+  TEST 3:  Register a new learner user.
+
+  EXPECTED RESULT:  Success (status 201).
+  */
+
+  let response = null;
+  let result = null;
+
+  sendRequest(learnerUserData, response, result);
+
+  expect(response?.ok).toBe(true);
+  expect(response?.status).toBe(201);
+  expect(["string", "number"].indexOf(typeof result?.userUID)).toBeGreaterThanOrEqual(0);
+  expect(typeof result?.msg).toBe("undefined");
+});
+
+/*****************************************************************************/
+
+test("Re-Register New Learner User", async function() {
+  /*
+  TEST 4:  Re-register the same learner.
+
+  EXPECTED RESULT:  Fail (status 403).
+  */
+
+  let response = null;
+  let result = null;
+
+  sendRequest(learnerUserData, response, result);
+
+  expect(response?.ok).toBe(false);
+  expect(response?.status).toBe(403);
   expect(typeof result?.msg).toBe("string");
   expect(typeof result?.userUID).toBe("undefined");
 });
