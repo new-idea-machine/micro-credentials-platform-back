2ab94acf73f711e565c640c2896294bcd28a20a8
Brandon <bbeam1@gmail.com>
2024-02-18 14:06:00 -0700

    fixed to be inline with open api

--- server.js
+++ server.js
@@ -13,21 +13,21 @@ const app = express();
 
 console.log(`Connected to ${connectionString}`);
 
-const userSchema = mongoose.Schema({ 
-    username: String, 
-    email: String, 
-    password: String, 
-    learnerData: {},
-    instructorData: {}
+const learnerSchema = new mongoose.Schema({});
+const instructorSchema = new mongoose.Schema({});
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    email: String,
+    password: String,
+    learnerData: mongoose.Schema({ data: { type: learnerSchema } }),
+    instructorData: mongoose.Schema({ data: { type: instructorSchema } })
 });
-const learnerSchema = mongoose.Schema({});
-const instructorSchema = mongoose.Schema({});
+
 
 const userModel = database.model("users", userSchema);
 const learnerModel = database.model("learner", learnerSchema);
 const instructorModel = database.model("instructor", instructorSchema);
-
-const users = await userModel.find();
 
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.use(bodyParser.json(), urlencodedParser);
@@ -38,48 +38,61 @@ app.listen(process.env.PORT, () => {
 });
 
 app.get("/", async (req, res) => {
-    try {
-    const users2 = await userModel.find();
-    console.log(users2)
-    res.json({ message: "Success" })}
-    catch (error) {
-        res.status(503).json({msg:'Cant reach server'})
+    try
+    {
+        const users = await userModel.find();
+        console.log(users)
+        res.json({ message: "Success" })
+    }
+    catch (error)
+    {
+        res.status(503).json({ msg: 'Cant reach server' })
     }
 });
 
 app.post("/user", async (req, res) => {
-    try{
-    const user = req.body
-    const validEmail = validator.isEmail(user.userInfo.email)
-    const takenEmail = await userModel.findOne({ email: user.userInfo.email })
-    if (takenEmail) {
-        res.status(403).json({msg:'User already exists (try logging in instead)'})
-    } else if (!validEmail) {
-        res.status(406).json({msg:'Invalid e-mail address'})
-    } else if (!user.password) {
-        res.status(406).json({msg:'Invalid password'})
+    try
+    {
+        const user = req.body
+        const validEmail = validator.isEmail(user.userInfo.email)
+        const takenEmail = await userModel.findOne({ email: user.userInfo.email })
+        if (takenEmail)
+        {
+            res.status(403).json({ msg: 'User already exists (try logging in instead)' })
+        } else if (!validEmail)
+        {
+            res.status(406).json({ msg: 'Invalid e-mail address' })
+        } else if (!user.password)
+        {
+            res.status(406).json({ msg: 'Invalid password' })
+        }
+        else
+        {
+            const registrant = new userModel({
+                username: user.userInfo.name,
+                email: user.userInfo.email,
+                password: user.password,
+                learnerData: new learnerModel({}),
+                instructorData: (user.isInstructor ? new instructorModel({}) : null)
+            })
+            const newDocument = await registrant.save()
+            res.status(201).json({ userUID: newDocument._id })
+        }
     }
-    else {
-        const registrant = new userModel({
-            username: user.userInfo.name,
-            email: user.userInfo.email,
-            password: user.password,
-            isInstructor: user.isInstructor
-        })
-        const newDocument = await registrant.save()
-        res.status(201).json({ userUID: newDocument._id })
-    }}
-    catch (error) {
-        res.status(503).json({msg:'Cant reach server'})
+    catch (error)
+    {
+        res.status(503).json({ msg: 'Cant reach server' })
     }
 });
 
 //Currently empties database, will change to only delete one user when done
 app.delete("/", async (req, res) => {
     await userModel.deleteMany({}).then((user) => {
-        if (!user) {
+        if (!user)
+        {
             res.status(400).send(req.params.Username + ' was not found');
-        } else {
+        } else
+        {
             res.status(200).send(req.params.Username + ' was deleted.');
         }
     })
@@ -88,5 +101,3 @@ app.delete("/", async (req, res) => {
             res.status(500).send('Error: ' + err);
         });
 });
-
-console.log(users);
