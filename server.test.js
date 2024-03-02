d0d11d96edc2f332799bc1245ae85f1b902848ea
Kevin Woodman <kwoodman1970@yahoo.ca>
2024-03-01 22:59:15 -0700

    Began adding user GET tests to server.test.js

--- server.test.js
+++ server.test.js
@@ -9,6 +9,13 @@ User POST Tests
 2. Register a new learner user with a bad password.
 3. Register a new learner user.
 4. Re-register the same learner user.
+
+User GET Tests
+---------------
+
+1. Get the same user as above.
+2. Get the same user using a bad password.
+3. Get a non-existent user.
 */
 
 /*
@@ -38,23 +45,36 @@ const learnerUserData = {
 
 /*********************************************************************************************/
 
-async function sendRequest(data) {
+async function sendRequest(method, data = null) {
   /*
   Handle all of the communication with the server.
 
-  "data" is the object to be converted to a JSON string and sent as the body of the request.
+  "method" is the HTTP request method (e.g. "GET" or "POST).
+
+  If "method" is "GET" then "data" is a string of query parameters; otherwise, "data" is the
+  object to be converted to a JSON string and sent as the body of the request.
 
   Return an array consisting of a "Response" object (or null if there was no response from the
   server) and the JSON object from the response's body (or null if the response's body didn't
   contain a JSON string).
   */
 
+  let parameters = "";
+
   const options = {
-    method: "POST",
-    mode: "cors",
-    headers: {"Content-Type": "application/json"},
-    body: JSON.stringify(data)
+    method,
+    mode: "cors"
   };
+
+  if (data) {
+    if (method !== "GET") {
+      options.headers = {"Content-Type": "application/json"};
+      options.body = JSON.stringify(data);
+    }
+    else {
+      parameters = "/" + data
+    }
+  }
 
   let response = null;                       // the response from the server (if any)
   let result = null;                         // the JSON object in the response's body (if any)
@@ -66,7 +86,7 @@ async function sendRequest(data) {
   */
 
   try {
-    response = await fetch(`${serverURL}/user`, options);
+    response = await fetch(`${serverURL}/user${parameters}`, options);
 
     try {
       result = await response.json();
@@ -95,7 +115,7 @@ test("Register New Learner User (Bad E-mail Address)", async function() {
 
   badData.userInfo.email = "Bad e-mail address";
 
-  const [response, result] = await sendRequest(badData);
+  const [response, result] = await sendRequest("POST", badData);
 
   expect(response?.ok).toBe(false);
   expect(response?.status).toBe(406);
@@ -116,7 +136,7 @@ test("Register New Learner User (Bad Password)", async function() {
 
   badData.password = null;
 
-  const [response, result] = await sendRequest(badData);
+  const [response, result] = await sendRequest("POST", badData);
 
   expect(response?.ok).toBe(false);
   expect(response?.status).toBe(406);
@@ -133,7 +153,7 @@ test("Register New Learner User", async function() {
   EXPECTED RESULT:  Success (status 201).
   */
 
-  const [response, result] = await sendRequest(learnerUserData);
+  const [response, result] = await sendRequest("POST", learnerUserData);
 
   expect(response?.ok).toBe(true);
   expect(response?.status).toBe(201);
@@ -150,10 +170,36 @@ test("Re-Register New Learner User", async function() {
   EXPECTED RESULT:  Fail (status 403).
   */
 
-  const [response, result] = await sendRequest(learnerUserData);
+  const [response, result] = await sendRequest("POST", learnerUserData);
 
   expect(response?.ok).toBe(false);
   expect(response?.status).toBe(403);
   expect(typeof result?.msg).toBe("string");
   expect(typeof result?.userUID).toBe("undefined");
 });
+
+// ============================================================================================
+// USER GET TESTS
+// ============================================================================================
+
+test("Get the Same User", async function() {
+  /*
+  TEST 1:  Get the same user as above.
+
+  EXPECTED RESULT:  Success (status 400).
+  */
+
+  const data = `email=${encodeURIComponent(learnerUserData.userInfo.email)}&` +
+               `password=${encodeURIComponent(learnerUserData.password)}`;
+
+  const [response, result] = await sendRequest("GET", data);
+
+  expect(response?.ok).toBe(true);
+  expect(response?.status).toBe(200);
+  expect(["string", "number"].indexOf(typeof result?.userUID)).toBeGreaterThanOrEqual(0);
+  expect(typeof result?.msg).toBe("undefined");
+  expect(result.email === learnerUserData.userInfo.email);
+});
+
+/*********************************************************************************************/
+
