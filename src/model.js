1163b5e67fd284a3405b3148964be24173d350df
Kevin Woodman <kwoodman1970@yahoo.ca>
2026-01-11 16:15:47 -0700

    Added a module/assessment discriminator to the Course schema in models.js and tweaked several other schemas

--- src/model.js
+++ src/model.js
@@ -18,7 +18,7 @@ import dotenv from "dotenv";
 import bcrypt from "bcrypt";
 
 /**
- * The the cost of processing the data for generating a hash.
+ * The cost of processing the data for generating a hash.
  *
  * See {@link https://www.npmjs.com/package/bcrypt#a-note-on-rounds bcrypt -- A Note on Rounds} for
  * more information on what this value means and how it affects performance.
@@ -144,42 +144,44 @@ userSchema.pre(
  * @property {Date} creationTime - When this question was created.
  * @property {Date} updateTime - When this question was last updated.
  */
-const questionSchema = new mongoose.Schema({
-  question: { type: String, required: true },
-  options: {
-    type: [String],
-    required: true,
-    validate: {
-      validator: (options) => {
-        return options.length >= 2 && options.length <= 26;
-      },
-      message: "There must be between 2 and 26 options"
-    }
+const questionSchema = new mongoose.Schema(
+  {
+    question: { type: String, required: true },
+    options: {
+      type: [String],
+      required: true,
+      validate: {
+        validator: function (options) {
+          return options.length >= 2 && options.length <= 26;
+        },
+        message: "There must be between 2 and 26 options"
+      }
+    },
+    answer: {
+      type: Number,
+      validate: {
+        validator: function (answer) {
+          return answer >= 0 && answer < this.options.length;
+        },
+        message:
+          "The index of the answer must be at least 0 and less than the number of options"
+      }
+    },
+    correctOption: {
+      type: Number,
+      required: true,
+      validate: {
+        validator: function (correctOption) {
+          return correctOption >= 0 && correctOption < this.options.length;
+        },
+        message:
+          "The index of the correct option must be at least 0 and less than the number of options"
+      }
+    },
+    explanation: { type: String, required: true }
   },
-  answer: {
-    type: Number,
-    validate: {
-      validator: (answer) => {
-        return answer >= 0 && answer < this.options.length;
-      },
-      message: "The index of the answer must be at least 0 and less than the number of options"
-    }
-  },
-  correctOption: {
-    type: Number,
-    required: true,
-    validate: {
-      validator: (correctOption) => {
-        return correctOption >= 0 && correctOption < this.options.length;
-      },
-      message:
-        "The index of the correct option must be at least 0 and less than the number of options"
-    }
-  },
-  explanation: { type: String, required: true },
-  creationTime: { type: Date, required: true, default: Date.now },
-  updateTime: { type: Date, required: true, default: Date.now }
-});
+  { timestamps: { createdAt: "creationTime", updatedAt: "updateTime" } }
+);
 
 /**
  * Module schema.
@@ -207,18 +209,20 @@ const moduleSchema = new mongoose.Schema({
   },
   chapters: [
     {
+      _id: false,
       title: { type: String, required: true },
       timeIndex: { type: Number, required: true } // in seconds
     }
   ],
   url: { type: String, required: true },
   urlAuthentication: {
-    scheme: String,
-    parameters: String
+    type: {
+      scheme: String,
+      parameters: String
+    },
+    required: false
   },
-  completed: Boolean,
-  creationTime: { type: Date, required: true, default: Date.now },
-  updateTime: { type: Date, required: true, default: Date.now }
+  completed: Boolean
 });
 
 /**
@@ -238,16 +242,38 @@ const assessmentSchema = new mongoose.Schema({
   currentQuestion: {
     type: Number,
     validate: {
-      validator: (currentQuestion) => {
-        return currentQuestion >= 0 && currentQuestion <= this.questions.length;
+      validator: function (currentQuestion) {
+        return currentQuestion >= 0 && currentQuestion < this.questions.length;
       },
       message:
         "The index of the current question must be at least 0 and less than the number of questions"
     }
-  },
-  creationTime: { type: Date, required: true, default: Date.now },
-  updateTime: { type: Date, required: true, default: Date.now }
+  }
 });
+
+/**
+ * Component schema.
+ *
+ * This allows Course components to be either Modules or Assessments and uses a discriminator to
+ * distinguish between them.
+ *
+ * @kind class
+ * @property {string} componentType - Discriminator key ("Module" or "Assessment").
+ */
+const componentSchema = new mongoose.Schema(
+  {
+    componentType: {
+      type: String,
+      required: true,
+      enum: ["Module", "Assessment"]
+    }
+  },
+  {
+    discriminatorKey: "componentType",
+    _id: false,
+    timestamps: { createdAt: "creationTime", updatedAt: "updateTime" }
+  }
+);
 
 /**
  * Course schema for the database.
@@ -264,38 +290,35 @@ const assessmentSchema = new mongoose.Schema({
  * @property {Date} creationTime - When this course was created.
  * @property {Date} updateTime - When this course was last updated.
  */
-const courseSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  instructor: { type: mongoose.Schema.Types.ObjectId, ref: "users", required: true },
-  components: {
-    type: [mongoose.Schema.Types.Mixed],
-    required: true,
-    validate: {
-      validator: function (component) {
-        return component.every(
-          (item) =>
-            (item.type && ["Audio", "Video", "Markdown"].includes(item.type)) || // Module
-            (item.questions && Array.isArray(item.questions)) // Assessment
-        );
-      },
-      message: "All components must be either Module or Assessment objects"
-    }
+const courseSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    instructor: { type: mongoose.Schema.Types.ObjectId, ref: "users", required: true },
+    components: [componentSchema],
+    currentComponent: {
+      type: Number,
+      validate: {
+        validator: function (currentComponent) {
+          return currentComponent >= 0 && currentComponent <= this.components.length;
+        },
+        message:
+          "The index of the current component must be at least 0 and no greater than the number of components"
+      }
+    },
+    credentialEarned: Boolean
   },
-  currentComponent: {
-    type: Number,
-    validate: {
-      validator: (currentComponent) => {
-        return currentComponent >= 0 && currentComponent <= this.components.length;
-      },
-      message:
-        "The index of the current component must be at least 0 and less than the number of components"
-    }
-  },
-  credentialEarned: Boolean,
-  creationTime: { type: Date, required: true, default: Date.now },
-  updateTime: { type: Date, required: true, default: Date.now }
-});
+  { timestamps: { createdAt: "creationTime", updatedAt: "updateTime" } }
+);
+
+/*
+The two schemas for the component discriminator are added here.
+*/
+
+const componentsPath = courseSchema.path("components");
+
+componentsPath.discriminator("Module", moduleSchema);
+componentsPath.discriminator("Assessment", assessmentSchema);
 
 /**
  * File schema for the database.
